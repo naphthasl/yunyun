91bce181b1409ad9363ff4588df0a62feeb928b1
Naphtha Nepanthez <naphtha@lotte.link>
2020-07-10 17:03:10 +0100

    Added header

---
+++ yunyun.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Yunyun is intended to be a simplified persistent data storage system similar
+to Python's built in shelve, but with features such as transparent file locking
+to allow for multi-threaded and multi-process access safely.
+"""
+
+__author__ = 'Naphtha Nepanthez'
+__version__ = '0.0.1'
+__license__ = 'MIT' # SEE LICENSE FILE
