5ac7a30728ba4cf09a20b4bb86640a0acb097568
Naphtha Nepanthez <naphtha@lotte.link>
2020-07-15 10:31:12 +0100

    Advance version

--- yunyun.py
+++ yunyun.py
@@ -10,7 +10,7 @@ License: MIT (see LICENSE for details)
 """
 
 __author__ = 'Naphtha Nepanthez'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 __license__ = 'MIT' # SEE LICENSE FILE
 __all__ = [
     'Interface',
