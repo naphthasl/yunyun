8f43c2a8d790e848526ddd891954d2007875671b
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-03 21:26:04 +0100

    Update test again and add array filtering

--- yunyun.py
+++ yunyun.py
@@ -1026,9 +1026,39 @@ class InstanceLockedShelve(Shelve):
 if __name__ == '__main__':
     import progressbar
     import matplotlib.pyplot as plt
+    import numpy
     
     FILENAME = '/tmp/test.yun'
     TEST_VALUES = [os.urandom(2 ** x) for x in range(16)]
+        
+    def smooth(x,window_len=64,window='hanning'):
+        x = numpy.array(x)
+        
+        if x.ndim != 1:
+            raise ValueError("smooth only accepts 1 dimension arrays.")
+        
+        if x.size < window_len:
+            raise ValueError("Input vector needs to be bigger than window.")
+        
+        if window_len<3:
+            return x
+        
+        types = ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']
+        if not window in types:
+            raise ValueError("Window is on of 'flat', 'hanning', 'hamming', " +
+                             "'bartlett', 'blackman'")
+        
+        s=numpy.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+        if window == 'flat': #moving average
+            w=numpy.ones(window_len,'d')
+        else:
+            w=eval('numpy.'+window+'(window_len)')
+        
+        y=numpy.convolve(w/w.sum(),s,mode='valid')
+        return y
+        
+    def tsmooth(x):
+        return smooth(x)[:len(x)]
         
     def pulverise_original():
         try:
@@ -1052,7 +1082,7 @@ if __name__ == '__main__':
             end = ((time.perf_counter() - start) / operations) * 1000
             times.append(end)
             
-        return indexes, times
+        return indexes, tsmooth(times)
         
     def testset(x, dotimes):
         indexes = list(range(dotimes))
@@ -1064,7 +1094,7 @@ if __name__ == '__main__':
             end = (time.perf_counter() - start) * 1000
             times.append(end)
             
-        return indexes, times
+        return indexes, tsmooth(times)
         
     def testget(x, dotimes):
         indexes = list(range(dotimes))
@@ -1076,7 +1106,21 @@ if __name__ == '__main__':
             end = (time.perf_counter() - start) * 1000
             times.append(end)
             
-        return indexes, times
+        return indexes, tsmooth(times)
+        
+    def testgt2(x, dotimes):
+        indexes = list(range(dotimes))
+        times = []
+        
+        for key in progressbar.progressbar(indexes):
+            value = os.urandom(8)
+            x[key] = value
+            start = time.perf_counter()
+            assert x[key] == value
+            end = (time.perf_counter() - start) * 1000
+            times.append(end)
+            
+        return indexes, tsmooth(times)
         
     def testin(x, dotimes):
         indexes = list(range(dotimes))
@@ -1088,7 +1132,7 @@ if __name__ == '__main__':
             end = (time.perf_counter() - start) * 1000
             times.append(end)
             
-        return indexes, times
+        return indexes, tsmooth(times)
     
     def testdel(x, dotimes):
         indexes = list(range(dotimes))
@@ -1100,7 +1144,7 @@ if __name__ == '__main__':
             end = (time.perf_counter() - start) * 1000
             times.append(end)
             
-        return list(reversed(indexes)), times
+        return list(reversed(indexes)), tsmooth(times)
     
     fig, ax = plt.subplots()
     
@@ -1109,11 +1153,15 @@ if __name__ == '__main__':
     pulverise_original()
     sobj = Shelve(FILENAME)
     ax.plot(*testset(sobj, els), label='set')
-    ax.plot(*testget(sobj, els), label='get')
-    ax.plot(*testin (sobj, els), label='in' )
-    ax.plot(*testdel(sobj, els), label='del')
+    ax.plot(*testget(sobj, els), label='get (after 16384 elements set)')
+    ax.plot(*testin (sobj, els), label='in (after 16384 elements set)' )
+    ax.plot(*testdel(sobj, els), label='del (after 16384 elements set)')
+    pulverise_original()
+    sobj = Shelve(FILENAME)
+    ax.plot(*testgt2(sobj, els), label='get (after each element set)')
     pulverise_original()
     with InstanceLockedShelve(FILENAME) as sobj:
+        sobj.trackkeys = False
         ax.plot(*testset(sobj, els), label='set_keyless')
     
     """
